109c50ac847c7de96b1262d81bb250f2cd96563f
Jaqueline Sobral <jaqueline.sobral@zup.com.br>
2024-12-31 12:29:38 -0300

    Adiciona novos métodos a interface

--- src/service/VideoService.java
+++ src/service/VideoService.java
@@ -6,5 +6,20 @@ import java.util.List;
 
 public interface VideoService {
     void addVideo(Video video);
-    List<Video> listVideos();
+
+    List<Video> listVideos(String query);
+
+    Video findVideoByTitle(String titulo);
+
+    boolean removeVideoList(Video video);
+
+    List<Video> filterByCategory(String categoria);
+
+    List<Video> sortByDate();
+
+    String getTotalVideos();
+
+    String getTotalDuration();
+
+    String getVideosByCategory();
 }

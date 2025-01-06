2d95ffbb7d5eb7a65bbddaf0ba726d1acb71ad90
Jaqueline Sobral <jaqueline.sobral@zup.com.br>
2025-01-06 19:51:21 -0300

    Adiciona a interface VideoService para gerenciamento de vídeos. - Método addVideo: Adiciona um novo vídeo. - Método listVideos: Lista vídeos com base em uma consulta. - Método updateVideo: Atualiza as informações de um vídeo existente.

--- src/service/VideoService.java
+++ src/service/VideoService.java
@@ -6,20 +6,6 @@ import java.util.List;
 
 public interface VideoService {
     void addVideo(Video video);
-
     List<Video> listVideos(String query);
-
-    Video findVideoByTitle(String titulo);
-
-    boolean removeVideoList(Video video);
-
-    List<Video> filterByCategory(String categoria);
-
-    List<Video> sortByDate();
-
-    String getTotalVideos();
-
-    String getTotalDuration();
-
-    String getVideosByCategory();
+    void updateVideo(Video video);
 }
